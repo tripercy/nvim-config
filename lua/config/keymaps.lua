0fca0ef8f1c5fac7676d655b8188ccb964ff117d
tripercy <percytri37@gmail.com>
2026-06-09 18:35:44 +0700

    fix: moved diagnostic keymaps to outside of LspAttach autocmd

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -46,11 +46,11 @@ vim.api.nvim_create_autocmd("LspAttach", {
         vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
         vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-        vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
     end,
 })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
 
 -- luasnip
 local ls = require("luasnip")
