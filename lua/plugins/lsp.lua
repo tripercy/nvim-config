0de4a6653e8469e03cc647dfd424b0bb7cfb496c
tripercy <percytri37@gmail.com>
2026-06-22 18:22:22 +0700

    feat: enable lombok for jdtls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,17 @@
+local function get_lombok_jar_path()
+    -- Configure jdtls BEFORE enabling it
+    local mason_registry = require("mason-registry")
+    local lombok_jar = ""
+
+    -- Safely resolve the mason-managed lombok path
+    if mason_registry.is_installed("jdtls") then
+        local jdtls_pkg = mason_registry.get_package("jdtls")
+        lombok_jar = jdtls_pkg:get_install_path() .. "/lombok.jar"
+    end
+
+    return lombok_jar
+end
+
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -17,6 +31,18 @@ return {
                 "bashls",
             }
         })
+
+        -- Intercept and configure jdtls natively
+        vim.lsp.config("jdtls", {
+            cmd = {
+                vim.fn.stdpath("data") .. "/mason/bin/jdtls",
+                string.format("--jvm-arg=-javaagent:%s", get_lombok_jar_path()),
+            },
+            root_dir = function(bufnr, on_dir)
+                on_dir(vim.fs.root(bufnr, { ".git", "pom.xml", "build.gradle", "build.gradle.kts" }))
+            end,
+        })
+
         require("mason-lspconfig").setup()
     end,
 }
